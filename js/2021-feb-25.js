5852a8af5f741697cf029ed37cfb3106643bdcac
Ashtar Paniagua <ashtarcodes@gmail.com>
2021-02-26 14:18:06 -0500

    solutions for yesterday feb 26

---
+++ js/2021-feb-25.js
@@ -0,0 +1,9 @@
+// reversed strings
+function solution(str){
+    return str.split('').reverse().join('')
+  }
+
+  //remove first and last
+  function removeChar(str){
+    return str.slice(1,-1)
+   };
