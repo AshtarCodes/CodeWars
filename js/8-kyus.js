b9a1cf5ee6bdcbf80a7ec5b4fe8480bdc55aabe6
Ashtar Paniagua <ashtarcodes@gmail.com>
2021-03-06 23:59:46 -0500

    more 8 kyu solutions

--- js/8-kyus.js
+++ js/8-kyus.js
@@ -12,4 +12,39 @@ function getAverage(marks){
     return Math.floor(marks.reduce((acc, i) => acc + i)/marks.length);
 };
 
-  
+
+// Count of positives / sum of negatives
+function countPositivesSumNegatives(input) {
+    if (!Array.isArray(input)){
+      return [];
+    };
+    let pos = 0;
+    let neg = [];
+    for (let num of input){
+      if (num > 0){
+        pos++
+      };
+      if (num < 0){
+        neg.push(num)
+      };
+    };    
+    let nSum = neg.reduce((sum, n)=>sum + n,0); 
+    return (pos > 0 && nSum < 0) ? [pos,nSum] : [];
+};
+
+// time since midnight in milliseconds
+function past(h, m, s){
+    //#Happy Coding! ^_^
+    let sum = (s * 1000) + (m * 60000) + (h * 3600000);
+    return sum;
+};
+
+// count up from a given number 
+function monkeyCount(n) {
+    // your code here
+      let numArr = [];
+      for (let i = 1; i <=n; i++){
+        numArr.push(i);
+      }
+      return numArr;
+};
