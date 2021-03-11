455e9e6fcf7fc81144f7e5ae1c77f2a537340a1d
Ashtar Paniagua <ashtarcodes@gmail.com>
2021-03-10 23:07:47 -0500

    7-kyus for today

--- js/7-kyus.js
+++ js/7-kyus.js
@@ -69,17 +69,51 @@ var customerData = {
     'Carrie': {
       visits: 4
     }
-  };
+};
   
-  function greetCustomer(firstName) {
+function greetCustomer(firstName) {
     // your code here
-    if (customerData[firstName] === undefined){
-        return 'Welcome! Is this your first time?';  
-    }; 
-    let customer = customerData[firstName];
-    if (customerData[firstName].visits === 1){
-        return(`Welcome back, ${firstName}! We\'re glad you liked us the first time!`);
-    } else if (customerData[firstName].visits > 1){
-        return(`Welcome back, ${firstName}! So glad to see you again!`);
-    };  
-  }
+   if (customerData[firstName] === undefined){
+       return 'Welcome! Is this your first time?';  
+   }; 
+   let customer = customerData[firstName];
+   if (customerData[firstName].visits === 1){
+       return(`Welcome back, ${firstName}! We\'re glad you liked us the first time!`);
+   } else if (customerData[firstName].visits > 1){
+       return(`Welcome back, ${firstName}! So glad to see you again!`);
+   };  
+};
+
+function isRubyComing(list) {
+    return list.some(x => x.language == 'Ruby')  
+};
+
+function getFirstPython(list) {
+    let dev = list.find(x => x.language === 'Python');
+    if (dev !== undefined) {return `${dev.firstName}, ${dev.country}`} else{return 'There will be no Python developers'}
+}; 
+
+function isSameLanguage(list) {
+    return list.every(x => x.language === list[0].language)
+};
+
+function getCount(st) {
+    let str = st.toLowerCase();
+    let vowelsCount = 0;  
+    let strArr = str.split('').filter(str => str.includes('a')||str.includes('e')||str.includes('i')||str.includes('o')||str.includes('u'))     
+    return vowelsCount + strArr.length;
+};
+
+function alienLanguage(str){
+    let words = str.split(' ') // arr of words
+    let WORDS = words.map(x => x.toUpperCase()) // arr of capital words
+    let result = []
+    // 
+    for (let i = 0; i < WORDS.length; i++){ // walks each word (i) l-r
+      let rev = WORDS[i].split('').reverse().join('') // 'MY' -> ' YM'
+      rev = rev.replace(rev[0], rev[0].toLowerCase()) // 'YM' -> 'yM'
+      result.push(rev.split('').reverse().join(''))  // 'yM' - 'My'  
+    }
+    return result.join(' ') // turn arr to str
+}
+
