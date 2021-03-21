7e46b819763e4a7c218e83e9e00b223396ed7da1
Ashtar Paniagua <ashtarcodes@gmail.com>
2021-03-21 08:41:26 -0400

    8 kyu

--- js/8-kyus.js
+++ js/8-kyus.js
@@ -502,3 +502,7 @@ function howMuchILoveYou(nbPetals) {
   let choices = ['I love you', 'a little', 'a lot', 'passionately', 'madly', 'not at all']
   return choices[(nbPetals-1) % choices.length];
 }
+
+function stringToArray(string){
+  return string.split(' ');
+}
