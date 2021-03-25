abd8b18e6f2987b8c14db209d6375f52dfa4b0bc
Ashtar Paniagua <ashtarcodes@gmail.com>
2021-03-25 08:46:55 -0400

    8kyu

--- js/8-kyus.js
+++ js/8-kyus.js
@@ -569,3 +569,25 @@ String.prototype.toAlternatingCase = function () {
   // Define your method here :)
   return this.split('').map(el => el === el.toUpperCase() ? el.toLowerCase() : el.toUpperCase()).join('')
 }
+
+// Hello function
+function greet (name, owner) {
+  if (name === owner) return 'Hello boss';
+  else if (name !== owner) return 'Hello guest';
+}
+
+// messi goals
+function goals (laLigaGoals, copaDelReyGoals, championsLeagueGoals) {
+  return laLigaGoals + copaDelReyGoals + championsLeagueGoals;
+}
+
+// Rock Paper Scissors
+const rps = (p1, p2) => {
+  if (p1 === 'scissors' && p2 === 'paper' || p1 === 'rock' && p2 === 'scissors' || p1 === 'paper' && p2 === 'rock'){
+    return 'Player 1 won!';
+  } else if (p1 === p2){
+    return 'Draw!'
+  } else {
+  return 'Player 2 won!';
+  }
+};
