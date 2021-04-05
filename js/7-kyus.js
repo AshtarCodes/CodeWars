2c42703b4c3bca903d564bb92c2de2dd2dc459fc
Ashtar Paniagua <ashtarcodes@gmail.com>
2021-04-05 08:46:17 -0400

    7 kyus

--- js/7-kyus.js
+++ js/7-kyus.js
@@ -127,3 +127,8 @@ function sum(){
       return acc;
     },0)
   }
+
+  //Square every digit and concat
+  function squareDigits(num){
+    return Number(String(num).split('').map(n => n * n).join(''))    
+  }
