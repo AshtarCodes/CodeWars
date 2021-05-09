4e560b8d148915258971f977c6b904535b6e13e6
Ashtar Paniagua <74083145+AshtarCodes@users.noreply.github.com>
2021-05-08 22:56:08 -0400

    added 8 kyu

--- js/8-kyus.js
+++ js/8-kyus.js
@@ -757,3 +757,11 @@ function gooseFilter (birds) {
 function superSize(num){
   return Number(String(num).split('').sort((a,b) => b - a).join(''));
 }
+
+const quarterOf = (month) => {
+  // Your code here
+  if (month <= 3) return 1;
+  if (month <= 6) return 2;
+  if (month <= 9) return 3;
+  if (month <= 12) return 4;
+}
