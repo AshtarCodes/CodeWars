68374b9b5286bc1a6baca95ea4dd0a7eebae0b8c
Ashtar Paniagua <ashtar.paniagua@gmail.com>
2021-05-20 00:32:18 -0400

    8 kyu

--- js/8-kyus.js
+++ js/8-kyus.js
@@ -942,3 +942,8 @@ function isLockNessMonster(s) {
 function hoopCount (n) {
   return n >= 10 ? "Great, now move on to tricks" : "Keep at it until you get it";  
 }
+
+//Watermelon
+function divide(weight){
+  return weight > 3 && !(weight % 2);
+}
