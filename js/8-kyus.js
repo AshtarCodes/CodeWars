b91bfab2345d127ebc11d2819264fbf9bad9d4cb
Ashtar Paniagua <ashtarcodes@gmail.com>
2021-05-24 08:22:13 -0400

    8 kyu

--- js/8-kyus.js
+++ js/8-kyus.js
@@ -942,3 +942,30 @@ function isLockNessMonster(s) {
 function getAge(inputString){
   return parseInt(inputString);
 }
+
+//Area or perimeter
+const areaOrPerimeter = function(l , w) {
+  if (l === w) {
+    return l * w;
+    } else if (l !== w) {
+      return (l + w) * 2;
+    }
+};
+
+// Switch it up
+function switchItUp(number){
+  let numberInText = {
+    0: 'Zero',
+    1: 'One',
+    2: 'Two',
+    3: 'Three',
+    4: 'Four',
+    5: 'Five',
+    6: 'Six',
+    7: 'Seven',
+    8: 'Eight',
+    9: 'Nine',
+  };
+
+  return numberInText[number];
+}
