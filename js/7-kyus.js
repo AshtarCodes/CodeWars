be39ef91b36058a5fec57027a3451ea1f569de3b
Ashtar Paniagua <74083145+AshtarCodes@users.noreply.github.com>
2021-06-17 11:51:03 -0400

    7 kyu

--- js/7-kyus.js
+++ js/7-kyus.js
@@ -190,3 +190,34 @@ function accum(s) {
   }).join('-')
   return result;	
 }
+
+//Coding Meetup #16 - Higher-Order Functions Series - Ask for missing details
+// P: arr of obj. R: Arr of obj with missing fields, and an inserted property
+// E: [ {firstName: null, question: 'Hi, could you please provide your firstName }] || []
+// Ps: loop through array, then loop through obj properties. find null. insert question.  
+function askForMissingDetails(list) {
+  let result = []
+  for (let dev of list){ // loop through each dev
+    for (let prop in dev){ // loop through dev properties
+      if(dev[prop] == null){
+        dev.question = `Hi, could you please provide your ${prop}.`
+        result.push(dev)
+      }
+    }
+  }
+  return result;
+}
+// refactored
+function askForMissingDetails(list) {
+  
+  return list.filter((dev) => {
+    
+    for (let prop in dev){ // loop through dev properties
+      if(dev[prop] == null){
+        dev.question = `Hi, could you please provide your ${prop}.`
+        return dev;
+      }
+    }
+  })
+  
+}
