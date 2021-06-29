9b215516985ce61474c071571673c0fd86a2ffb8
Ashtar Paniagua <74083145+AshtarCodes@users.noreply.github.com>
2021-06-29 16:17:19 -0400

    6 kyu

--- js/6-kyus.js
+++ js/6-kyus.js
@@ -400,3 +400,27 @@ function findOutlier(n){
 function filter_list(l) {
   return l.filter(el => typeof el === 'number');
 }
+
+/*
+The goal of this exercise is to convert a string to a new string where each character in the new string is "(" if that character appears only once in the original string, or ")" if that character appears more than once in the original string. Ignore capitalization when determining if a character is a duplicate.
+*/
+function duplicateEncode(word){
+  word = word.toLowerCase();
+  
+  let result = "";
+  
+  let count = {}
+  for (let c of word.split('')){
+    if (!count[c]) count[c] = 0;
+    count[c]++;
+  }
+  
+  for (let char of word){
+    if(count[char] && count[char] > 1){
+      result += ')';
+    } else if (count[char] === 1){
+      result += '(';
+    }
+  }
+  return result;
+};
