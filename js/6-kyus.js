50fa3c760f9cc7a8cc6e1027b58011724e57f96f
Ashtar Paniagua <74083145+AshtarCodes@users.noreply.github.com>
2021-07-01 23:25:38 -0400

    6 kyu

--- js/6-kyus.js
+++ js/6-kyus.js
@@ -459,3 +459,22 @@ function order(words){
 
   return sorted.join(' ').trim();
 }
+
+/*
+Complete the method/function so that it converts dash/underscore delimited words into camel casing. The first word within the output should be capitalized only if the original word was capitalized (known as Upper Camel Case, also often referred to as Pascal case).
+*/
+function toCamelCase(str){
+  let result = "";
+
+  for (let i = 0; i < str.length; i++){
+    
+    if (str[i -1] === '-' || str[i-1] === '_') {
+      result += str[i].toUpperCase();
+    } else if (str[i] === '-' || str[i] === '_'){
+      continue;
+    } else {
+      result += str[i];      
+    }    
+  }
+  return result;
+}
