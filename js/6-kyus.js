fd4fb4596c9c5b78e534747968119aa65ed136ed
Ashtar Paniagua <74083145+AshtarCodes@users.noreply.github.com>
2021-07-11 21:18:39 -0400

    6 kyu

--- js/6-kyus.js
+++ js/6-kyus.js
@@ -615,3 +615,22 @@ function anagrams(word, words) {
 }
 
 console.log(anagrams('abba', ['aabb', 'abcd', 'bbaa', 'dada']), ['aabb', 'bbaa']);
+
+/* You live in the city of Cartesia where all roads are laid out in a perfect grid. You arrived ten minutes too early to an appointment, so you decided to take the opportunity to go for a short walk. The city provides its citizens with a Walk Generating App on their phones -- everytime you press the button it sends you an array of one-letter strings representing directions to walk (eg. ['n', 's', 'w', 'e']). You always walk only a single block for each letter (direction) and you know it takes you one minute to traverse one city block, so create a function that will return true if the walk the app gives you will take you exactly ten minutes (you don't want to be early or late!) and will, of course, return you to your starting point. Return false otherwise. */
+function isValidWalk(walk) {
+  if (walk.length !== 10) return false;
+  
+  let count = walk.reduce((acc,c) => {
+    if (!acc[c]){
+      acc[c] = 0;  
+    } 
+    acc[c]++;
+    return acc;
+  },{})
+  if (count['n'] !== count['s'] || count['w'] !== count['e']){
+    return false;
+  }
+  return true;
+};
+console.log(isValidWalk(['n','s','n','s']), true);
+console.log(isValidWalk(['n']), false);
