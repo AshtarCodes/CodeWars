836ff30259e9f06ea942d1eba28564af58a0f396
Ashtar Paniagua <74083145+AshtarCodes@users.noreply.github.com>
2021-07-23 18:17:18 -0400

    7 kyu

--- js/7-kyus.js
+++ js/7-kyus.js
@@ -290,3 +290,13 @@ console.log(removeConsecutiveDuplicates("alpha beta beta gamma gamma"), 'alpha b
 function solution(str, ending){
   return str.endsWith(ending);
 }
+
+/* 
+Given a string of words (x), you need to return an array of the words, sorted alphabetically by the final character in each.
+If two words have the same last letter, they returned array should show them in the order they appeared in the given string.
+*/
+function last(x){
+  return x.split(' ').sort((a,b) => {
+    return a[a.length-1] > b[b.length-1] ? 1 : -1;
+  })
+};
