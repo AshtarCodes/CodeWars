509d6feaf0e8d368c44def29bed62c3aeac0eedf
Ashtar Paniagua <74083145+AshtarCodes@users.noreply.github.com>
2022-06-12 17:35:57 -0400

    adds some recursion problem set solutions

---
+++ js/recursion.js
@@ -0,0 +1,45 @@
+function power(base, exponent){
+    if (exponent === 0) return 1;
+    return base * power(base, (exponent - 1));
+}
+
+function factorial(num) {
+     const cache = {};
+     function factorialHelper(num){
+        if (num === 0) return 1;
+        if (cache[num]) return cache[num];
+        const result = num * factorialHelper(num-1);
+        cache[num] = result;
+       return result;
+    }
+    return factorialHelper(num);
+}
+
+function productOfArray (arr){
+    if (arr.length === 0) return 1;
+    return arr[0] * productOfArray(arr.slice(1));
+}
+
+function recursiveRange(num){
+    if (num === 0) return num;
+    return num + recursiveRange(num-1);
+}
+
+function fibonacci(num) {
+     const cache = {};
+    function fib(num){
+      // add whatever parameters you deem necessary - good luck!  
+      if (num === 2 || num === 1) return 1;
+      if (cache[num]) return cache[num];
+      let result = fib(num-1) + fib(num-2);
+      cache[num] = result;
+      return result;
+    }
+    return fib(num);
+}
+
+function reverseString(str){
+    // add whatever parameters you deem necessary - good luck!
+    if (str === '') return '';
+    return reverse(str.slice(1)) + str[0];
+  }
